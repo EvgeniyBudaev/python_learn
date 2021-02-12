93999e43cfbba07a6e23ad740de3b4f9d36ad205
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-02-12 08:12:11 +0300

    Декоратор @wraps

--- function/decorators.py
+++ function/decorators.py
@@ -55,3 +55,21 @@ def factorial(num):
   print(math.factorial(num))
 
 factorial(100)
+
+# Декоратор @wraps
+
+from functools import wraps
+
+def log_decorator_with_wraps(func):
+  @wraps(func)
+  def wrap(*args, **kwargs):
+    print(f'Calling func {func}')
+    func(*args, **kwargs)
+    print(f'Func {func} finished its work')
+  return wrap
+
+@log_decorator_with_wraps
+def hello_decorator_with_wraps():
+  print('hello, decorator_with_wraps!')
+
+hello_decorator_with_wraps()   
