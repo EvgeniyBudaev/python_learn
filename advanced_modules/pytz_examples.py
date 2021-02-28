52a8022cdb1a2fe18b826715bfca0e8d1081d4e8
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-02-28 08:31:33 +0300

    Извлечение информации о timezone

---
+++ advanced_modules/pytz_examples.py
@@ -0,0 +1,16 @@
+# Необходимо установить pytz
+# import pytz
+import datetime
+
+# kiev = 'Europe/Kiev'
+# tz_kiev = pytz.timezone(kiev)
+# kiev_time = datetime.datetime.now(tz=tz_kiev)
+# print(kiev_time)
+# for tz pytz.add_timezones:
+#   print(tz)
+#   for country in pytz.country_names:
+#     print(country, pytz.country_names[country])
+
+print(datetime.datetime.today()) # 2021-02-28 07:57:53.683730
+print(datetime.datetime.now()) # 2021-02-28 07:57:53.683824
+print(datetime.datetime.utcnow()) # 2021-02-28 04:57:53.683832
