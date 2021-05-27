1a5385e3412832dd9017536dad1140138143600e
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2021-05-27 05:11:18 +0300

    unittest & TDD to Calculator

--- test/yandex_test/unittest/calculator/calculator.py
+++ test/yandex_test/unittest/calculator/calculator.py
@@ -2,10 +2,16 @@ class Calculator:
     """Производит арифметические действия."""
     def summ(self, *args):
         """Возвращает сумму принятых аргументов."""
+        if len(args) < 2:
+            return None
         sum = 0
         for i in args:
             sum += i
         return sum
+
+    def divider(self, num1, num2):
+        """Возвращает результат деления num1 / num2."""
+        return num1 / num2
 
     def square(self, num):
         """Возвращает квадратный корень аргумента."""
--- test/yandex_test/unittest/calculator/test_calculator.py
+++ test/yandex_test/unittest/calculator/test_calculator.py
@@ -10,12 +10,17 @@ from test.yandex_test.unittest.calculator.calculator import Calculator
 class TestCalc(unittest.TestCase):
     """Тестируем Calculator."""
 
-    @classmethod
-    def setUp(cls):
+    def setUp(self):
         """Подготовка прогона теста. Вызывается перед каждым тестом."""
         # Arrange - подготавливаем данные для теста
         # Создаем экземпляр класса Calculator()
-        cls.calc = Calculator()
+        self.calc = Calculator()
+    # @classmethod
+    # def setUpClass(cls):
+    #     """Подготовка прогона теста. Вызывается перед каждым тестом."""
+    #     # Arrange - подготавливаем данные для теста
+    #     # Создаем экземпляр класса Calculator()
+    #     cls.calc = Calculator()
 
     def test_summ(self):
         # summ возвращает сумму принятых аргументов
@@ -25,6 +30,23 @@ class TestCalc(unittest.TestCase):
 
         # Assert - проверяем, что метод работает
         self.assertEqual(act, 5, 'Метод summ работает неправильно')
+
+    def test_summ_no_argument(self):
+        act = self.calc.summ()
+        self.assertIsNone(act, 'Метод summ работает неправильно')
+
+    def test_summ_one_argument(self):
+        act = self.calc.summ(5)
+        self.assertIsNone(act, 'Метод summ работает неправильно')
+
+    def test_divider(self):
+        act = self.calc.divider(4, 2)  # вызовите метод divider с аргументом
+        self.assertEqual(act, 2, 'текст, если проверка провалена')
+
+    def test_divider_zero_division(self):
+        # Проверьте, что деление на 0 выбрасывает исключение
+        with self.assertRaises(ZeroDivisionError):
+            act = self.calc.divider(4, 0)
 
     def test_square(self):
         # square() возвращает квадратный корень аргумента
